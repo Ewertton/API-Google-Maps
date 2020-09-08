6e1062b867468d7ca7876522914caa4fd4d9f69f
Ewertton <ewertton.klaytton@hotmail.com>
2020-09-07 23:17:28 -0300

    code update and documentation

--- javascript.js
+++ javascript.js
@@ -1,58 +1,87 @@
-var myLatLng = {lat: -5.79448,lng: -35.211};
-var mapOptions = {
+
+const myLatLng = {
+    lat: -5.79448,
+    lng: -35.211
+};
+
+const mapOptions = {
   center: myLatLng,
   zoom: 13,
   mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 
-var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+const options = {
+    types :'cities',
+    radius :'100'
+}
+
+const from = document.getElementById("from");
+const to = document.getElementById("to");
+
+const map = new google.maps.Map(
+    document.getElementById('googleMap'), 
+    mapOptions
+);
+
+const directionsService = new google.maps.DirectionsService();
+const directionsDisplay = new google.maps.DirectionsRenderer();
 
 
-var directionsService = new google.maps.DirectionsService();
+const autoCompleteFrom = new google.maps.places.Autocomplete(
+    from, 
+    options.types, 
+    options.radius
+);
 
-var directionsDisplay = new google.maps.DirectionsRenderer();
+const autoCompleteTo = new google.maps.places.Autocomplete(
+    to, 
+    options.types, 
+    options.radius
+);
 
 directionsDisplay.setMap(map);
 
-
 function calcRoute(){
-    var request = {
+
+    //CODIGO PARA PODE ESCOLHER O TIPO DE VIAGEM DRIVING WALKING, BYCYCLING, TRANSIT
+    //const selectedMode = document.getElementById("mode").value;
+
+    const request = {
         origin: document.getElementById('from').value,
         destination: document.getElementById('to').value,
-        travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
+        travelMode: google.maps.TravelMode.DRIVING, //[selectedMode], //WALKING, BYCYCLING, TRANSIT
         unitSystem: google.maps.UnitSystem.METRIC //IMPERIAL
     }
-    
-    directionsService.route(request,
-    function(result,status){
-     if(status == google.maps.DirectionsStatus.OK){
-         $("#output").html("<div class='alert-info'>From: "+document.getElementById("from").value+".<br/>To: "+document.getElementById("to").value+" "+result.routes[0].legs[0].distance+".<br/>Durations: " +result.routes[0].legs[0].duration.text+".</div>"); 
-      
-         
-         directionsDisplay.setDirections(result);
-         
-     }else{
-         
-         directionsDisplay.setDirections({routes:[]});
-         
-         map.setCenter(myLatLng);
-         
-         $("#output").html("<div class='alert-danger'>Could not retrive distance</div>");
-     }  
-    });   
+
+    function respRoute(result,status){
+
+        const response ={
+            form: document.getElementById("from").value, //INPUT DO HTML
+            to: document.getElementById("to").value,     //INPUT DO HTML 
+            distanceText: result.routes[0].legs[0].distance.text,
+            durationsText: result.routes[0].legs[0].duration.text
+            //distanceValue: result.routes[0].legs[0].distance.value, DISTANCIA EM METROS
+            //durationsValue: result.routes[0].legs[0].duration.value, TEMPO EM SEGUNDOS
+        }
+
+        if(status == "OK"){
+            $("#output").html(
+                "<div class='alert-info'>From: "+response.form+
+                ".<br/>To: "+response.to+"<br/> Driving Distance: "+response.distanceText+
+                ".<br/>Durations: "+response.durationsText+"</div>"
+                ); 
+                console.log(result);
+            directionsDisplay.setDirections(result);
+        }
+
+        else{
+            directionsDisplay.setDirections({routes:[]});
+            map.setCenter(myLatLng);
+            $("#output").html("<div class='alert-danger'>Could not retrive distance</div>");
+        }  
+    }
+    directionsService.route(request, respRoute);   
 }
-
-var options = {
-    radius:['(3000)'],
-    types:['(regions)']
-}
-var distance = {radius:['(10000)']}
-
-var input1 = document.getElementById("from");
-var autocomplete1 = new google.maps.places.Autocomplete(input1, options,distance);
-
-var input2 = document.getElementById("to");
-var autocomplete2 = new google.maps.places.Autocomplete(input2, options,distance);
 
 
 
